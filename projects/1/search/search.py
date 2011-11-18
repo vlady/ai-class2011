81766d6c536b51ba38e6c2b6c9de2ccc5ec233e0
Vlad Dumitriu <vladut.mail@gmail.com>
2011-11-18 07:57:40 +0200

    DFS,BFS and UFC algorithms (first 3 questions)

--- projects/1/search/search.py
+++ projects/1/search/search.py
@@ -84,20 +84,96 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from game import Directions
+    from util import Stack
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+
+    frontier = Stack()
+    frontier.push(problem.getStartState())
+    reached = {}
+    reached[problem.getStartState()] = None
+
+    path = []
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            # backtrack the steps
+            while reached[state] is not None:
+                action_pack = reached[state]
+                state = action_pack[0]
+                path.append(action_pack[1])
+            path.reverse()
+            break
+        for s in problem.getSuccessors(state):
+            next_state = s[0]
+            if reached.has_key(next_state):
+                continue
+            action = s[1]
+            reached[next_state] = (state, action)
+            frontier.push(next_state)
+    return path
 
 def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first.
     [2nd Edition: p 73, 3rd Edition: p 82]
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from util import Queue
+    frontier = Queue()
+    frontier.push(problem.getStartState())
+    reached = {}
+    reached[problem.getStartState()] = None
+
+    path = []
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            # backtrack the steps
+            while reached[state] is not None:
+                action_pack = reached[state]
+                state = action_pack[0]
+                path.append(action_pack[1])
+            path.reverse()
+            break
+        for s in problem.getSuccessors(state):
+            next_state = s[0]
+            if reached.has_key(next_state):
+                continue
+            action = s[1]
+            reached[next_state] = (state, action)
+            frontier.push(next_state)
+    return path
 
 def uniformCostSearch(problem):
     "Search the node of least total cost first. "
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from util import PriorityQueue
+    frontier = PriorityQueue()
+    frontier.push(problem.getStartState(), 0)
+    reached = {}
+    reached[problem.getStartState()] = (None, None, 0)
+
+    path = []
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            # backtrack the steps
+            while reached[state][0] is not None:
+                action_pack = reached[state]
+                state = action_pack[0]
+                path.append(action_pack[1])
+            path.reverse()
+            break
+        for s in problem.getSuccessors(state):
+            next_state = s[0]
+            if reached.has_key(next_state):
+                continue
+            action = s[1]
+            cost = s[2] + reached[state][2]
+            reached[next_state] = (state, action, cost)
+            frontier.push(next_state, cost)
+    return path
 
 def nullHeuristic(state, problem=None):
     """
